550d8ca1cb77e23bd22540d33157d8af27c043d6
joehall87 <joehall87@gmail.com>
2021-04-14 14:00:58 +0100

    Improving upload script

--- scripts/populate_db.py
+++ scripts/populate_db.py
@@ -27,56 +27,55 @@ _ROOT_URL = "https://raw.githubusercontent.com/openscriptures/morphhb/master/wlc
 @click.option("--limit", default=31, help="Limit number of records uploaded to dynamodb.")
 def run(limit):
     """Parse openscriptures xml-files and make my own json ones."""
-    logging.info("Downloading openscriptures xml files")
-    _download_files()
-    logging.info("Parsing openscriptures xml files")
-    _parse_files()
-    logging.info(f"Upload to dynamodb (limited to {limit} records)")
-    _upload(limit)
+    uploaded = 0
+    for book_id in _BOOK_IDS:
+        logging.info(f"Working on {book_id}")
+        _download_file(book_id)
+        _parse_file(book_id)
+        uploaded = _upload(book_id, uploaded, limit)
+        logging.info(f"Uploaded {uploaded} records")
+        if uploaded >= limit:
+            logging.warning(f"Reached limit of {limit} - stopping!")
+            break
 
 
-def _download_files():
+def _download_file(book_id):
     dir_ = _CACHE_DIR / "b3-heb-raw"
     dir_.mkdir(parents=True, exist_ok=True)
-    for book_id in _BOOK_IDS:
-        path = dir_ / f"{book_id}.xml"
-        if not path.exists():
-            logging.info(f"Requesting {book_id}")
-            r = requests.get(f"{_ROOT_URL}/{book_id}.xml")
-            with path.open('wb') as f:
-                f.write(r.content)
+    path = dir_ / f"{book_id}.xml"
+    if not path.exists():
+        url = f"{_ROOT_URL}/{book_id}.xml"
+        logging.info(f"Requesting {url}")
+        r = requests.get(url)
+        with path.open('wb') as f:
+            f.write(r.content)
 
 
-def _parse_files():
+def _parse_file(book_id):
     dir_ = _CACHE_DIR / "b3-heb"
     dir_.mkdir(parents=True, exist_ok=True)
-    for book_id in _BOOK_IDS:
-        path = dir_ / f"{book_id}.json"
-        logging.info('Creating {}'.format(path))
-        tree = _parse_xml(book_id)
-        tokens = list(_tokenize(book_id, tree))
-        grouped = list(_group_tokens(tokens))
-        with path.open("w", encoding="utf8") as f:
-            json.dump(grouped, f)
+    path = dir_ / f"{book_id}.json"
+    tree = _parse_xml(book_id)
+    tokens = list(_tokenize(book_id, tree))
+    grouped = list(_group_tokens(tokens))
+    with path.open("w", encoding="utf8") as f:
+        json.dump(grouped, f)
 
 
-def _upload(limit):
+def _upload(book_id, uploaded, limit):
     dynamodb = boto3.resource("dynamodb")
     table = dynamodb.Table('b3-books')
-    dir_ = _CACHE_DIR / "b3-heb"
-    count, actual = 0, 0
+    path = _CACHE_DIR / "b3-heb" / f"{book_id}.json"
     with table.batch_writer() as batch:
-        for path in dir_.glob("*.json"):
-            with path.open("r", encoding="utf8") as f:
-                records = json.load(f)
-            logging.info(f"Uploading {len(records)} from {path.name}")
-            for record in records:
-                count += 1
-                if count > limit:
-                    break
-                actual += 1
-                batch.put_item(Item=record)
-    logging.info(f"Uploaded {actual} records")
+        with path.open("r", encoding="utf8") as f:
+            records = json.load(f)
+        logging.info(f"Uploading {len(records)} from {path.name}")
+        for record in records:
+            batch.put_item(Item=record)
+            uploaded += 1
+            if uploaded >= limit:
+                break
+    return uploaded
 
 
 def _parse_xml(book_id):
@@ -137,4 +136,4 @@ def _parse_osis_id(ref):
 
 
 if __name__ == '__main__':
-    run()
+    run()  # pylint: disable=no-value-for-parameter
