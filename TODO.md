32e0267e69b6cd2d1c1a12975c49bbfd5c290d6e
joehall87 <joehall87@gmail.com>
2021-04-22 16:59:57 +0100

    Oops, add parser code

--- TODO.md
+++ TODO.md
@@ -1,9 +1,9 @@
 # TODO
-1. Parse USFX english for strongs!
-2. Fix spacing
-4. Populate strongs definitions (store in separate table initially)
-5. Create modals
-8. Bring lambda api code into codebase (and possibly autodeploy...)
-9. Greek!
-10. (STRETCH!) Statistical correlation highlighting of Hebrew/Greek
-11. Call it a day!
+1. Ignore stopwords for english
+2. Fix undefined issue
+3. Populate strongs definitions (store in separate table initially)
+4. Create modals
+5. Bring lambda api code into codebase (and possibly autodeploy...)
+6. Greek!
+7. (STRETCH!) Statistical correlation highlighting of Hebrew/Greek
+8. Call it a day!
---
+++ b3/parser/usfx.py
@@ -0,0 +1,91 @@
+import itertools
+from pathlib import Path
+import re
+import xml.etree.ElementTree as ET
+
+
+def parse_usfx(path):
+    """
+    Parse mental USFX format to big list of tokenized verses.
+    """
+    with Path(path).open("r", encoding="utf8") as f:
+        xmlstr = f.read()
+        xmlstr = re.sub(r" xmlns=['\"][^'\"]+['\"]", "", xmlstr, count=1)
+        xmlstr = re.sub(r"</?wj>", "", xmlstr)
+    tree = ET.fromstring(xmlstr)
+    token_iter = _iter_tokens(tree)
+    grouper = itertools.groupby(token_iter, key=lambda x: x[0])
+    records = [
+        {
+            "chapterId": cid,
+            "verseNum": vnum,
+            "tokens": [tokens for _, tokens in group],
+        }
+        for (cid, vnum), group in grouper
+    ]
+    return records
+    
+    
+def _iter_tokens(tree):
+    """
+    Iterate over every token in every verse.
+    """
+    vid = None
+    for e in tree.iter():  # iterates recursively through the doc
+        # Handle verse count
+        if e.tag in {"book", "ve"}:
+            vid = None
+        elif e.tag == "v":
+            vid = _extract_verse_id(e)
+        # Handle tokens
+        if vid and e.tag in {"w"}:
+            token = {"type": "w", "text": e.text}
+            if "s" in e.attrib:
+                token["strongs"] = e.attrib["s"].split()
+            yield vid, token
+        if vid and e.tag in {"p", "q", "qs"} and e.text:
+            yield vid, {"type": "o", "text": e.text.replace("\n", " ")}
+        if vid and e.tag in {"w", "f"} and e.tail:
+            yield vid, {"type": "o", "text": e.tail.replace("\n", " ")}
+            
+            
+def _extract_verse_id(e):
+    bookid, cnum, vnum = e.attrib["bcv"].split(".")
+    cid = f"{_usfx_to_osis(bookid)}.{cnum}"
+    vnum = int(vnum)
+    return cid, vnum
+
+
+def _usfx_to_osis(usfx_id):
+    """
+    Normalize book refs to OSIS.
+    """
+    return {
+        # Torah
+        "Exo": "Exod",
+        "Deu": "Deut",
+        # Neviim
+        "Jos": "Josh",
+        "Jdg": "Judg",
+        "1Sa": "1Sam",
+        "2Sa": "2Sam",
+        "1Ki": "1Kgs",
+        "2Ki": "2Kgs",
+        "Ezk": "Ezek",
+        "Jol": "Joel",
+        "Amo": "Amos",
+        "Oba": "Obad",
+        "Jon": "Jonah",
+        "Zep": "Zeph",
+        "Zec": "Zech",
+        # Ketuvim
+        "Psa": "Ps",
+        "Pro": "Prov",
+        "Sng": "Song",
+        "Rut": "Ruth",
+        "Ecc": "Eccl",
+        "Est": "Esth",
+        "Ezr": "Ezra",
+        "1Ch": "1Chr",
+        "2Ch": "2Chr",
+    }.get(usfx_id.title(), usfx_id.title())
